98df137eae6808b3c0a4a95c2ea88118c0a83edd
liusashmily <liusashmily@gmail.com>
2013-11-03 22:13:19 -0600

    2 02 Tool Folder Creator Add MenuItem

---
+++ Lab3/Assets/Editor/MakeProjectFolder.js
@@ -0,0 +1,21 @@
+﻿// Generate folders in our project
+
+#pragma strict
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+// Add menu item
+// Generate folders and names from the script
+@MenuItem("Project Tools/Make Folders")
+
+static function MakeFolder()
+{
+	Debug.Log("Test");
+}
