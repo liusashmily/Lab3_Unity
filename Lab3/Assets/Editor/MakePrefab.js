1acbca03ba2660d15febe5d5a737ad696bb28dab
liusashmily <liusashmily@gmail.com>
2013-11-11 18:49:35 -0600

    3 03 Tool Prefab Creator CreateEmptyPrefab

--- Lab3/Assets/Editor/MakePrefab.js
+++ Lab3/Assets/Editor/MakePrefab.js
@@ -2,13 +2,22 @@
 
 @MenuItem ("Project Tools/Make Prefab")
 
-static function CreatePrefab()
-{
-	Debug.Log("Prefab");
+static function CreatePrefab() {
+	// Debug.Log("Prefab");
 
 	var selectedObjects :GameObject[] = Selection.gameObjects;
 
 	for(var go : GameObject in selectedObjects){
-		print(go.name);
+		//print(go.name);
+		var name : String = go.name;
+		var localPath : String = "Assets/" + name + ".prefab";
+		print(localPath);
 	}
+	createNew(localPath);
+}
+
+static function createNew (localPath : String) {
+	var prefab : Object = PrefabUtility.CreateEmptyPrefab(localPath);
+
+
 }
