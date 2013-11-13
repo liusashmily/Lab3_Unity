555c272d4d3bcc2d0a6945f4951dccf747df6f91
liusashmily <liusashmily@gmail.com>
2013-11-12 21:35:46 -0600

    3 06 Tool Prefab Creator Comment Review

--- Lab3/Assets/Editor/MakePrefab.js
+++ Lab3/Assets/Editor/MakePrefab.js
@@ -5,30 +5,51 @@
 static function CreatePrefab() {
 	// Debug.Log("Prefab");
 
+	// Select from the scene view
 	var selectedObjects :GameObject[] = Selection.gameObjects;
 
+	// Loop through our selection
 	for(var go : GameObject in selectedObjects){
 		//print(go.name);
+
+		// Store the name of our selection
 		var name : String = go.name;
+
+		// Create the path for the prefab
 		var localPath : String = "Assets/Prefabs/" + name + ".prefab";
+
+		// Check for object in project
 		if(AssetDatabase.LoadAssetAtPath(localPath, GameObject))
 		{
+			// Check for user choice
 			if(EditorUtility.DisplayDialog("Caution", "Prefab already exits. Overwrite?", "Yes", "No")){
+				// Create a new prefab
 				createNew(go, localPath);
 			}
 			print("Do Not Create!");
 
 		}
 		else{
+				// Create a new prefab
 				createNew(go, localPath);
 		}
 	}
 }
 
+// Create a new prefab
 static function createNew (selectedObjects : GameObject, localPath : String) {
+	// Store prefab
 	var prefab : Object = PrefabUtility.CreateEmptyPrefab(localPath);
+
+	// Set prefab to prefab
 	PrefabUtility.ReplacePrefab(selectedObjects, prefab);
+
+	// Refresh the database
 	AssetDatabase.Refresh();
 
+	// Remove the selected object
+	DestroyImmediate(selectedObjects);
 
+	// Replace object with prefab
+	var clone: GameObject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
 }
