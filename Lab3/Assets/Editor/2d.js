b7dd8755f07d82f895c358f1b995a45d892ca049
liusashmily <liusashmily@gmail.com>
2013-11-14 23:36:31 -0600

    research

---
+++ Lab3/Assets/Editor/2d.js
@@ -0,0 +1,34 @@
+﻿/*
+Five step coding for the beginner
+
+Step 1: Tool Description
+- Description: We are creating a script that will animate a 2d sprite sheet
+- Functionally, we'll have control of the speed for the animation, the frames, placement,
+- Tool side: Accessibe through the inspector
+- Support for the diffuse, spec and the normal maps
+- Tile sheet support for the level editing
+- Support for individual charcter peices
+- Any other ideas
+
+
+Step 2: Research
+- List of topics
+- 2d sprite textures
+- 2d array
+- x, y coordinators
+- u, v coordinators
+- speed maybe fps
+- offsetting to move u, v
+- Time
+- variable and their type
+- Function 
+- Materials and different types or maps
+- Words that we don't know
+
+Step 3: Instruction Manual
+
+Step 4: Pseudocode
+
+Step 5: Write it already
+
+*/
