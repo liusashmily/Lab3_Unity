b90cec71b3bcd46456d5e8e18d009e0455858a05
liusashmily <liusashmily@gmail.com>
2013-11-27 07:40:57 -0600

    step 3 uv row column

--- Lab3/Assets/Scripts/step 3.js
+++ Lab3/Assets/Scripts/step 3.js
@@ -1,9 +1,12 @@
-﻿#pragma strict
-
-function Start () {
-
-}
+﻿var tileX : int;		// u
+var tileY : int;		// v
 
 function Update () {
+	renderer.material.mainTextureOffset = Vector2(1, 0);
+
+	var size = Vector2(1.0/tileX, 1.0/tileY);
+
+	print(size);
+	renderer.material.mainTextureScale	= size;
 
 }
