70b7656006ba554b6bf6c4a3d3eb2eafd407db66
liusashmily <liusashmily@gmail.com>
2013-11-29 08:43:20 -0600

    step 5

--- Lab3/Assets/Scripts/step 5.js
+++ Lab3/Assets/Scripts/step 5.js
@@ -1,9 +1,24 @@
-﻿#pragma strict
+﻿var column : int;						// X(u) coordinate
+var row : int;							// Y(v) coordinate
+// var index : int = 1;
+var framePerSecond = 8.0;				// Speed of animation
 
-function Start () {
+function Update(){
+	// Time Control fps
+	var index : int = Time.time * framePerSecond;
 
-}
+	// Modulate
+	index = index % (column * row);
 
-function Update () {
+	// Scale
+	var size = Vector2 (1.0 / column, 1.0 / row);
 
+	// Offset
+	var offset = Vector2 ( index * size.x, row);
+
+	// Texture Offset
+	renderer.material.mainTextureOffset = offset;
+
+	// Texture Scale
+	renderer.material.mainTextureScale = size;
 }
