6a683757f375eb860f7fdda9bf1c956df4877d15
liusashmily <liusashmily@gmail.com>
2013-12-01 06:59:47 -0600

    step 8

--- Lab3/Assets/Scripts/step 8.js
+++ Lab3/Assets/Scripts/step 8.js
@@ -1,9 +1,36 @@
-﻿#pragma strict
+﻿var columnSize : int;						// X(u) coordinate
+var rowSize : int;							// Y(v) coordinate
+var framePerSecond = 8.0;					// Speed of animation
 
-function Start () {
+var rowFrameStart : int = 0;
+var colFrameStart : int = 0;
+var totalFrames : int = 1;
 
-}
 
-function Update () {
+function Update(){
+	// Time Control fps
+	var index : int = Time.time * framePerSecond;
 
+	// Modulate
+	index = index % totalFrames;
+
+	print(index);
+
+	// Scale
+	var size = Vector2 (1.0 / columnSize, 1.0 / rowSize);
+
+	var u : int = index % columnSize;
+	var v : int = index / rowSize;
+
+	// Offset
+	var offset = Vector2 ( (u + colFrameStart ) * size.x, ( v + rowFrameStart) * size.y);
+
+	// Texture Offset
+	renderer.material.mainTextureOffset = offset;
+
+	// Texture Scale
+	renderer.material.mainTextureScale = size;
+
+	renderer.material.SetTextureOffset ("_BumpMap", offset);
+	renderer.material.SetTextureScale  ("_BumpMap", size);
 }
