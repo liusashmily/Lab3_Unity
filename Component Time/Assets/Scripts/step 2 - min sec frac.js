d6dbbd1fc9faa9023117bb5084878855b6994776
liusashmily <liusashmily@gmail.com>
2013-12-03 07:21:06 -0600

    step 2 min sec frac

--- Component Time/Assets/Scripts/step 2 - min sec frac.js
+++ Component Time/Assets/Scripts/step 2 - min sec frac.js
@@ -1,5 +1,27 @@
+var playTime	 : float = 0.0;
+var days 		 : float = 0.0;
+var hours		 : float = 0.0;
+var minutes		 : float = 0.0;
+var seconds		 : float = 0.0;
+var fractions	 : float = 0.0;
 
 function Update ()
 {
+	playTime = Time.time;
+	seconds = playTime % 60;
+	fractions = playTime * 10 % 10;
+	minutes = playTime / 60 % 60;
+	hours = playTime / 3600 % 24;
+	days = hours / 24 % 365;
+
+	// print("Minutes: " + minutes + " Seconds: " + seconds + " Fractions: " + fractions);
+
+	if( seconds >= 10){
+		print("You are at TEN seconds mark.");
+	}
 	
+	if ( minutes >= 1) {
+		print("You are at the one munite mark.");
+	}
+
 }
