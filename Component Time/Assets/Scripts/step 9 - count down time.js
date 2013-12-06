2bb92191b08b07321b1b31f12ab5bfb7e63b3546
liusashmily <liusashmily@gmail.com>
2013-12-06 08:05:42 -0600

    step 9 count down

--- Component Time/Assets/Scripts/step 9 - count down time.js
+++ Component Time/Assets/Scripts/step 9 - count down time.js
@@ -1,5 +1,33 @@
+// Count Down Time
+// Reset the current time or stopped time to zero
+
+var playTime		: float = 0.0;
+var countDownDelay	: float = 0.0;
+var countDownAmount : float = 0.0;
+
+var timeActive 		: boolean = false;
 
 function Update ()
 {
-	
+	if (timeActive) {													// enable time
+		playTime = countDownDelay - Time.time + countDownAmount;											// playTime is current time since start
+	}
+
+	if (Input.GetKeyDown ("7")) {
+		countDownDelay = Time.time;
+		timeActive = true;
+	}
+
+	if (playTime < 0)
+	{
+		timeActive = false;
+		//playTime = 0.0;
+	}
+}
+
+function OnGUI () {
+
+	GUILayout.Label ("Play Time: " + playTime);
+
+
 }
