21f13a68a18875b00e066ace56cee6f57b1a0979
liusashmily <liusashmily@gmail.com>
2013-12-05 21:06:42 -0600

    step 7 continue time

--- Component Time/Assets/Scripts/step 7 - continue time.js
+++ Component Time/Assets/Scripts/step 7 - continue time.js
@@ -1,5 +1,33 @@
+// Continue Time
+// Continue timer from stopped time
+
+var playTime		: float = 0.0;
+var stopTime		: float = 0.0;
+var continueTime	: float = 0.0;
+
+var timeActive 		: boolean = true;
 
 function Update ()
 {
+	if (timeActive) {									// enable time
+		playTime = Time.time - continueTime;							// playTime is current time since start
+	}
 	
+	if (Input.GetKeyDown ("3")) {
+		stopTime = Time.time;
+		timeActive = false;
+	}
+
+	if (Input.GetKeyDown ("5")) {
+		continueTime = Time.time - playTime;
+		timeActive = true;
+	}
+}
+
+function OnGUI () {
+	GUILayout.Label ("PlayTime: " + playTime);
+	GUILayout.Label ("Stop Time: " + stopTime);
+	GUILayout.Label ("Continue Time: " + continueTime);
+
+
 }
