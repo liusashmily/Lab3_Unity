f73d8ef58be7735126c7a7f76bc485adbd48f2a8
liusashmily <liusashmily@gmail.com>
2013-12-06 08:25:46 -0600

    step 10 delay time

--- Component Time/Assets/Scripts/step 10 - delay time.js
+++ Component Time/Assets/Scripts/step 10 - delay time.js
@@ -1,5 +1,29 @@
+// Delay Time
+// Desc: Continue timer after delayed amount
+
+var playTime		: float = 0.0;
+var delayTime		: float = 0.0;
+var delayedAmount	: float = 0.0;
+
+var timeActive 		: boolean = true;
 
 function Update ()
 {
-	
+	if (timeActive) {													// enable time
+		playTime =  Time.time;											// playTime is current time since start
+	}
+
+	// delayTime = 10 + 3; playTime > 13
+	if ( playTime > delayTime ) {
+		delayTime = Time.time + delayedAmount;
+		timeActive = true;
+	}
+
+}
+
+function OnGUI () {
+
+	GUILayout.Label ("Play Time: " + playTime);
+	GUILayout.Label ("Delay Time: " + delayTime.ToString("f0"));
+
 }
