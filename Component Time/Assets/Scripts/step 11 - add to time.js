7121b7a9bdcefc5da242cdefd32c193393ddda75
liusashmily <liusashmily@gmail.com>
2013-12-07 10:10:37 -0600

    add to time

--- Component Time/Assets/Scripts/step 11 - add to time.js
+++ Component Time/Assets/Scripts/step 11 - add to time.js
@@ -1,5 +1,30 @@
+// Add to Time
+// Desc: Add more time to the timer : 1. once; 2. continous adding
+
+var playTime		: float = 0.0;
+var addToTimeAmount	: float = 0.0;
+var timeAmount      : float = 0.0;
+
+var timeActive 		: boolean = true;
 
 function Update ()
 {
-	
+	if (timeActive) {													// enable time
+		playTime =  Time.time + addToTimeAmount;						// playTime is current time since start
+	}
+
+	if ( Input.GetKeyDown("8") ) {										// Add once
+		addToTimeAmount = timeAmount;
+	}
+
+	if (Input.GetKeyDown("9")) {										// Add continous
+		addToTimeAmount += timeAmount;
+	}
+
+}
+
+function OnGUI () {
+
+	GUILayout.Label ("Play Time: " + playTime);
+
 }
