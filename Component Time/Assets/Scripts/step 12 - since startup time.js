80b060869d10128904e14edcae77659f7f4d9abb
liusashmily <liusashmily@gmail.com>
2013-12-07 16:08:33 -0600

    step 12 since startup time

--- Component Time/Assets/Scripts/step 12 - since startup time.js
+++ Component Time/Assets/Scripts/step 12 - since startup time.js
@@ -1,5 +1,37 @@
+// Since Start Time
+// Desc: Real time since start up
+
+var playTime		: float = 0.0;
+var actualTime		: float = 0.0;
+
+var timeActive 		: boolean = true;
 
 function Update ()
 {
-	
+	if (timeActive) {													// enable time
+		playTime =  Time.time;						// playTime is current time since start
+	}
+
+	if (Input.GetKeyDown ("0"))
+	{
+		actualTime = Time.realtimeSinceStartup;
+	}
+
+	if (Input.GetKeyDown ("2"))
+	{
+		Time.timeScale = 0.0;
+	}
+
+	if (Input.GetKeyUp ("2"))
+	{
+		Time.timeScale = 1.0;
+	}
+
+}
+
+function OnGUI () {
+
+	GUILayout.Label ("Play Time: " + playTime);
+	GUILayout.Label ("Actual Time: " + actualTime);
+
 }
